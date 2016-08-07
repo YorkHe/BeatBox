a2610c49a6530f90d8c9fce32deccfcc0c73d734
York <kiddmagician@gmail.com>
2016-08-07 22:57:50 +0800

    Initial commit

---
+++ app/src/main/java/com/york42/beatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package com.york42.beatbox;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends SingleFragmentActivity{
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/york42/beatbox/Sound.java
@@ -0,0 +1,24 @@
+package com.york42.beatbox;
+
+/**
+ * Created by 宇 on 2016/8/7.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath(){
+        return mAssetPath;
+    }
+
+    public String getName(){
+        return mName;
+    }
+}
